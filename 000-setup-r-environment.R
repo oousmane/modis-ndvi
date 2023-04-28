881e03a17d464d1e9d99d52e6f78cc333d2798a7
oousmane <ouedraoogo.ousmane@outlook.com>
2023-04-28 17:36:12 +0100

    setup script & tif files moving

---
+++ 000-setup-r-environment.R
@@ -0,0 +1,33 @@
+# Download required packages. remove the packages if needed.
+
+if (!require("pacman")) install.packages("pacman")
+if (!require("pak")) install.packages("pak") 
+
+options(timeout = 600) # increase downloading timeout to 600 seconds
+pak::pkg_install(
+  c(
+    "tidyverse",
+    "terra",
+    "sf",
+    "stars",
+    "sfheaders",
+    "MODIStsp",
+    "ggspatial",
+    "wmgeolab/rgeoboundaries"
+  )
+)
+
+# Load required libraries 
+
+pacman::p_load(
+  c(
+    tidyverse,
+    terra,
+    sf,
+    stars,
+    sfheaders,
+    MODIStsp,
+    ggspatial,
+    rgeoboundaries
+  )
+)
--- 002-download-ndvi.R
+++ 002-download-ndvi.R
@@ -17,7 +17,7 @@ MODIStsp(
   out_folder = "VegetationData",
   out_folder_mod = "VegetationData",
   selprod = "Vegetation Indexes_16Days_250m (M*D13Q1)",
-  bandsel = c("NDVI","EVI"), # select band of interest 
+  bandsel = "NDVI", # select band of interest 
   user = "", #eartdata username
   password = "", #earthdata password
   start_date = "2001.01.01", # start_date modify as needed
@@ -33,6 +33,11 @@ cat(
   /VegetationData/bfa_admbnda_adm0_igb_20200323/VI_16Days_250m_v6/NDVI/"
 )
 
-  
+## Moving all processed geotiff files into ndvi-tif/
 
- 
+ if(!fs::dir_exists("ndvi-tif")) fs::dir_create("ndvi-tif")
+
+path_to_tifs <- "~/modis-ndvi/VegetationData/bfa_admbnda_adm0_igb_20200323/VI_16Days_250m_v6/NDVI"
+
+tif_files <- fs::dir_ls(path = path_to_tifs,glob = "*.tif")
+fs::file_move(path = tif_files,new_path = "ndvi-tif/")

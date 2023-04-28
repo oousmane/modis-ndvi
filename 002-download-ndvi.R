77b202883ca3ad1df7405ce65bb03c48bd61fe61
oousmane <ouedraoogo.ousmane@outlook.com>
2023-04-28 03:03:56 +0100

    download modis

--- 002-download-ndvi.R
+++ 002-download-ndvi.R
@@ -1,14 +1,23 @@
 library(tidyverse)
 library(MODIStsp)
 
-MODIStsp_get_prodlayers("M*D13Q1")
 
+MODIStsp_get_prodlayers("M*D13Q1") # see bands names
+
+# create output directory if needed
+
+ if(!fs::dir_exists("VegetationData")) fs::dir_create("VegetationData")
+
+# Modis Vegetation indices downloading ! Pay attention to area and/or 
+# period of interest. Files can be really huge.
+
+print("---- Start Vegetation Data Downloading -----")
 MODIStsp(
   gui = FALSE,
   out_folder = "VegetationData",
   out_folder_mod = "VegetationData",
   selprod = "Vegetation Indexes_16Days_250m (M*D13Q1)",
-  bandsel = "NDVI",
+  bandsel = c("NDVI","EVI"), # select band of interest 
   user = "", #eartdata username
   password = "", #earthdata password
   start_date = "2001.01.01", # start_date modify as needed
@@ -19,6 +28,11 @@ MODIStsp(
   out_format = "GTiff"
 )
 
+cat(
+  "All files were downloaded, see *.tif files here:
+  /VegetationData/bfa_admbnda_adm0_igb_20200323/VI_16Days_250m_v6/NDVI/"
+)
 
   
+
  

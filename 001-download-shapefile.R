ae6056c97ee7de0359a90ced1d1f47e2928e0f2e
oousmane <ouedraoogo.ousmane@outlook.com>
2023-04-28 02:45:52 +0100

    Initial commit

--- 001-download-shapefile.R
+++ 001-download-shapefile.R
@@ -1,5 +1,5 @@
-# Download burkina shapefiles for data.humdata.org (source is igb)
-# one time
+# Download burkina shapefiles from data.humdata.org (source is igb)
+# one time execution
 url <- "https://data.humdata.org/dataset/2940ed80-4b69-4b98-abaf-af79088852c5/resource/636d259b-22ba-4ecb-8702-ef58b38f14dd/download/bfa_adm_igb_20200323_shp.zip"
 
 download.file(
@@ -11,3 +11,4 @@ unzip(
   zipfile = "bfa_adm_igb_20200323_shp.zip",
   exdir ="bfa_adm_igb_20200323_shp" 
 )
+
--- 002-download-ndvi.R
+++ 002-download-ndvi.R
@@ -9,50 +9,16 @@ MODIStsp(
   out_folder_mod = "VegetationData",
   selprod = "Vegetation Indexes_16Days_250m (M*D13Q1)",
   bandsel = "NDVI",
-  user = "ousmane_earthdata", #eartdata username
-  password = "sodpac-xihrur-2veHcu", #earthdata password
-  start_date = "2001.01.01",
-  end_date = "2001.01.31",
+  user = "", #eartdata username
+  password = "", #earthdata password
+  start_date = "2001.01.01", # start_date modify as needed
+  end_date = "2001.01.31", # end_date modify as needed
   verbose = FALSE,
   spatmeth = "file",
   spafile = "VegetationData/shapefile/bfa_admbnda_adm0_igb_20200323.shp",
   out_format = "GTiff"
 )
 
-data <- rast(x="VegetationData/bfa_admbnda_adm0_igb_20200323/VI_16Days_250m_v6/NDVI/MOD13Q1_NDVI_2001_001.tif")
 
-bf1 <- sf::read_sf("VegetationData/shapefile/bfa_admbnda_adm0_igb_20200323.shp")
-
-data <- terra::project(data,"EPSG:4326")
-
-masked <- mask(data,bf)/10000
-
-ggplot()+
-  geom_spatraster(
-    data = masked,
-  )+
-  geom_sf(
-    data = bf,
-    fill =NA,
-    linewidth = 1
-  )+
-  scale_fill_terrain_b(
-    na.value = NA,
-    breaks = c(-Inf,0,0.2,0.3,0.5,Inf)
-  )
-
-raster_ts <- load(
-  file = "VegetationData/bfa_admbnda_adm0_igb_20200323/VI_16Days_250m_v6/Time_Series/RData/Terra/NDVI/MOD13Q1_NDVI_1_2001_17_2001_RData.RData"
-  ) %>% 
-  get() %>% 
-  rast()
-
-n <- raster_ts %>% 
-  varnames() 
-  
-function(x){
-  str_split(n,pattern = )
-  
-}
   
  
